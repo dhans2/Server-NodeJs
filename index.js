93151a6b69eb8a15ab0ebfc762ed6847a77d406a
DESKTOP-33LK62J\HP <risyadhana2@gmail.com>
2022-03-17 12:48:45 +0700

    Commit

---
+++ index.js
@@ -0,0 +1,21 @@
+require('dotenv').config();
+
+const express = require('express');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const userRoutes = require('./routes/user');
+const categoryRoutes = require('./routes/category');
+const subcategoryRoutes = require('./routes/sub_category');
+
+const app = express();
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use("/api/v1/user", userRoutes);
+app.use("/api/v1/category", categoryRoutes);
+app.use("/api/v1/subcategory", subcategoryRoutes);
+app.use('/uploads/profile_picture', express.static('uploads/profile_picture'));
+app.listen(3000, () => {
+    console.log('server run di port ');
+});
